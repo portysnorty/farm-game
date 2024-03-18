42e6420b6b3c173676dbc4f587837cc22b37a454
portysnorty <port.snort@icloud.com>
2024-03-18 08:46:39 -0600

    finished but gonna add more
    Co-authored-by: Spider-Tree <Spider-Tree@users.noreply.github.com>

--- First_Window_Project/main.cpp
+++ First_Window_Project/main.cpp
@@ -1,8 +1,8 @@
 //Our setup for now is a bit unique so here is an example of how to include the SDL libraries in our projects
 #define SDL_MAIN_HANDLED
-#include "..\..\SDL2\headers\SDL.h"
-#include "..\..\SDL2\headers\SDL_image.h"
-#include "..\..\SDL2\headers\SDL_mixer.h"
+#include <SDL2\SDL.h>
+#include <SDL2\SDL_image.h>
+#include <SDL2\SDL_mixer.h>
 
 #include <iostream>
 #include <string>
@@ -16,6 +16,8 @@
 #include "giraffe.h"
 #include "sheep.h"
 
+using namespace std;
+//create the global variables
 namespace SDL_globals
 {
     const int SCREEN_WIDTH = 1920;
@@ -43,7 +45,6 @@ namespace SDL_globals
     Mix_Chunk* sound_monster = NULL;
 }
 
-using namespace std;
 using namespace SDL_globals;
 
 Animal* grab_animal(int t){
@@ -56,6 +57,7 @@ Animal* grab_animal(int t){
 }
 
 int main(){
+
     animals.push_back(new Sheep("Woolie"));
     animals.push_back(new Pig("Puck"));
     animals.push_back(new Cow("Bessie"));
@@ -65,7 +67,7 @@ int main(){
     SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
 
     window = SDL_CreateWindow("the game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
     screen_surface = SDL_GetWindowSurface(window);
 
     // Setting the position of the text surface
@@ -81,9 +83,11 @@ int main(){
     // Adding the farm scene
     image_surface = SDL_LoadBMP("Farmies.bmp");
     SDL_BlitSurface(image_surface, NULL, screen_surface, NULL);
-    music_jazz= SDL_LoadMUS("Jazz.mp3");
+    
+    music_jazz= Mix_LoadMUS("Jazz.mp3");
+    Mix_PlayMusic(music_jazz, -1);
+
     SDL_UpdateWindowSurface(window);
-    Mix_PlayChannel(-1,music_jazz,0);
     int whichAnimal = 0;
     Animal* the_animal = grab_animal(whichAnimal);
 
@@ -97,7 +101,7 @@ int main(){
                 int x, y;
                 SDL_GetMouseState( &x, &y );
 
-                string is_click = " ";
+                bool is_click = false;
                 
                 the_animal = animals[whichAnimal];
 
@@ -105,7 +109,8 @@ int main(){
                     //sheep
                     case(0):
                         if(((x>=1000)&&(x<=1121)) && ((y>=530)&&(y<=730))){
-                            is_click = "First.bmp";
+                            is_click = true;
+                            image_surface = SDL_LoadBMP("First.bmp");
                         }else{
                             cout<<"\nThat is not "<<the_animal->name<< ".\n";
                             cout<<"I can't belive you messed up.\n";
@@ -117,7 +122,8 @@ int main(){
                     //pig
                     case(1):
                         if(((x>=0)&&(x<=65)) && ((y>=530)&&(y<=730))){
-                            is_click = "Half_Way.bmp"
+                            is_click = true;
+                            image_surface = SDL_LoadBMP("Half_Way.bmp");
                         }else{
                             cout<<"\nThat is not "<<the_animal->name<<".\n";
                             cout<<"You are treading on dangerous ground.\n";
@@ -131,7 +137,8 @@ int main(){
                     //cow
                     case(2):
                         if(((x>=325)&&(x<=440)) && ((y>=285)&&(y<=335))){
-                            is_click = "3_Progress.bmp";
+                            is_click = true;
+                            image_surface = SDL_LoadBMP("3_Progress.bmp");
                         }else{
                             cout<<"\nThat is not "<<the_animal->name<<".\n";
                             cout<<"is_click\n";
@@ -143,7 +150,8 @@ int main(){
                     //chicken
                     case(3):
                         if(((x>=1670)&&(x<=1715)) && ((y>=111)&&(y<=143))){
-                            is_click = "Full_Progress.bmp"
+                            is_click = true;
+                            image_surface = SDL_LoadBMP("Full_Progress.bmp");
                         }else{
                             cout<<"\nThat is not "<<the_animal->name<<".\n";
                             cout<<"DIE\n";
@@ -178,12 +186,11 @@ int main(){
                         break;
                 }
 
-                if(is_click!=" "){
+                if(is_click){
                     cout<<"\nthat was "<<the_animal->name<<"\n";
                     cout<<"Nice job\n\n";
                     whichAnimal++;
                     the_animal = grab_animal(whichAnimal);
-                    image_surface = SDL_LoadBMP(is_click);
 
                     SDL_BlitSurface(image_surface, NULL, screen_surface, &yopos);
                     
